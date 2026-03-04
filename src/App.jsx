462e57703e8c2e6fab4c256209ca4896cb6078b6
Alabi Oluwafunmilayo <kkoredesemi@gmail.com>
2026-03-04 11:41:10 +0100

    Fix imports and folder casing for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// Import Navbar and Footer
+// Navbar and Footer
 import Navbar from "./components/Navbar"; 
 import Footer from "./components/Footer";
 
 // Public pages
 import Home from './pages/Home';
 import WhoWeAre from './pages/whoweare/WhoWeAre';
+import OurHistory from './pages/whoweare/OurHistory';
+import QuickFacts from './pages/whoweare/QuickFacts';
+import OurLeadership from './pages/whoweare/OurLeadership';
+import WhatWeBelieve from './pages/whoweare/WhatWeBelieve';
+import MissionVision from './pages/whoweare/MissionVision';
+
 import Sermons from './pages/Sermons';
 import ExploreHeritage from './pages/Explore/ExploreHeritage';
 import AmNewHere from './pages/Explore/AmNewHere';
@@ -16,61 +22,49 @@ import Testimony from './pages/Explore/Testimony';
 import ContactUs from './pages/Explore/ContactUs';
 import PrayerRequest from './pages/Explore/PrayerRequest';
 import Counselling from './pages/Explore/Counselling';
+import UpdateProfilePage from './pages/Explore/UpdateProfilePage';
+import Birthday from './pages/Explore/Birthday';
 import Giving from './pages/Giving';
 import Icare from './pages/Icare';
-import UpdateProfilePage from './pages/Explore/UpdateProfilePage';
-import LiveService from './pages/Liveservice';
-
-import OurHistory from './pages/whoweare/Ourhistory';
-import QuickFacts from './pages/whoweare/Quickfacts';
-import OurLeadership from './pages/whoweare/Ourleadership';
-import WhatWeBelieve from './pages/whoweare/Whatwebelieve';
-import MissionVision from './pages/whoWeAre/MissionVision';
-import Birthday from './pages/Explore/Birthday';
+import LiveService from './pages/LiveService';
+import FellowshipPage from './pages/Fellowship';
 
 // Admin pages
 import Login from './pages/admin/Login';
-import Dashboard from "./pages/admin/Dashboard";
-
-import PrivateAdminRoute from './pages/admin/PrivateAdminroute';
-
-// dynamic 
-import FellowshipPage from './pages/Fellowship';
+import Dashboard from './pages/admin/Dashboard';
+import PrivateAdminRoute from './pages/admin/PrivateAdminRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
-        {/* Navbar at the top */}
         <Navbar />
-
-        {/* Main content area */}
         <main className="main-content">
           <Routes>
             {/* Public routes */}
             <Route path="/" element={<Home />} />
             <Route path="/who-we-are" element={<WhoWeAre />}>
-            <Route path="history" element={<OurHistory />} />
-            <Route path="facts" element={<QuickFacts />} />
-            <Route path="leadership" element={<OurLeadership />} />
-            <Route path="believe" element={<WhatWeBelieve />} />
-            <Route path="mission-vision" element={<MissionVision />} />
-           </Route>
+              <Route path="history" element={<OurHistory />} />
+              <Route path="facts" element={<QuickFacts />} />
+              <Route path="leadership" element={<OurLeadership />} />
+              <Route path="believe" element={<WhatWeBelieve />} />
+              <Route path="mission-vision" element={<MissionVision />} />
+            </Route>
+
             <Route path="/sermons" element={<Sermons />} />
             <Route path="/live-service" element={<LiveService />} />
-           <Route path="/explore-heritage" element={<ExploreHeritage />}>
-            <Route path="am-new-here" element={<AmNewHere />} />
-            <Route path="testimony" element={<Testimony />} />
-            <Route path="contact-us" element={<ContactUs />} />
-             <Route path="prayer-request" element={<PrayerRequest />} />
-             <Route path="birthday" element={<Birthday />} />
-             <Route path="update-profile" element={<UpdateProfilePage />} />
-             <Route path="counselling" element={<Counselling />} />
+            <Route path="/explore-heritage" element={<ExploreHeritage />}>
+              <Route path="am-new-here" element={<AmNewHere />} />
+              <Route path="testimony" element={<Testimony />} />
+              <Route path="contact-us" element={<ContactUs />} />
+              <Route path="prayer-request" element={<PrayerRequest />} />
+              <Route path="birthday" element={<Birthday />} />
+              <Route path="update-profile" element={<UpdateProfilePage />} />
+              <Route path="counselling" element={<Counselling />} />
             </Route>
+
             <Route path="/icare" element={<Icare />} />
-          
             <Route path="/giving" element={<Giving />} />
-            
             <Route path="/fellowships/:type" element={<FellowshipPage />} />
 
             {/* Admin routes */}
@@ -83,42 +77,16 @@ function App() {
                 </PrivateAdminRoute>
               }
             />
-            {/* <Route
-              path="/admin/members"
-              element={
-                <PrivateAdminRoute>
-                  <Members />
-                </PrivateAdminRoute>
-              }
-            /> */}
-            {/* <Route
-              path="/admin/events"
-              element={
-                <PrivateAdminRoute>
-                  <AdminEvents />
-                </PrivateAdminRoute>
-              }
-            /> */}
-            {/* <Route
-              path="/admin/prayers"
-              element={
-                <PrivateAdminRoute>
-                  <Prayers />
-                </PrivateAdminRoute>
-              }
-            /> */}
-             <Route
+            <Route
               path="/admin/testimony"
               element={
                 <PrivateAdminRoute>
                   <Testimony />
                 </PrivateAdminRoute>
               }
-            /> 
+            />
           </Routes>
         </main>
-
-        {/* Footer at the bottom */}
         <Footer />
       </div>
     </BrowserRouter>
